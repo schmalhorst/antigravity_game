3d91f66bb21c881da41a872e64345b09653ea166
Schmally <jonathan.schmalhorst@gmail.com>
2025-12-05 17:50:57 -0600

    chore: Update debug build artifacts.

--- AntigravityMoon/obj/Debug/net9.0/AntigravityMoon.AssemblyInfo.cs
+++ AntigravityMoon/obj/Debug/net9.0/AntigravityMoon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AntigravityMoon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce1bf64b8caa205a91cc912221f80eb4ac17747e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2aeb3344ea9654a27550e948eecf7786dee098e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AntigravityMoon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AntigravityMoon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
